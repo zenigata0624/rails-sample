50f22d74f6d561f5c5608e3cb08dc6fac54f6588
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-02-24 03:12:25 +0000

    [finish]sample

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,42 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+  list = List.new(list_params)
+  list.save
+  flash[:notice] = "投稿は成功しました"
+    redirect_to "/lists"
+  end
+
+  def index
+     @lists = List.all
+  end
+
+  def show
+   @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title,:body,:image)
+  end
+
+end
---
+++ app/models/list.rb
@@ -0,0 +1,9 @@
+class List < ApplicationRecord
+  has_one_attached :image
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  
+  # コメントアウトする
+   validates :image, presence: true
+end
